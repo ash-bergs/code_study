ff9faa41c21777b79e762f3845ba6dbd2bb72fba
Ash <65979049+ashley-bergsma@users.noreply.github.com>
2021-03-02 11:54:01 -0800

    function stringy()
    return a string of alternating 0's and 1's for a given size (int)

--- str_challenges.js
+++ str_challenges.js
@@ -160,3 +160,43 @@ The `repeat()` method constructs and returns a new string which contains the spe
 I provided the range paramater by using strNum.lenth - i - 1 - the result of that expression is the COUNT paramter - it adjusts for the proper number of zeroes. 
 */
 /* -------------------------------------------------------------------------- */
+
+// ❓ "Stringy"
+/* ------------------------------- description ------------------------------ */
+/* 
+write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+
+the string should start with a 1.
+
+a string with size 6 should return :'101010'.
+
+with size 4 should return : '1010'.
+
+with size 12 should return : '101010101010'.
+
+The size will always be positive and will only use whole numbers.
+*/
+
+function stringy(size) {
+  // init var to push results onto
+  const result = []; 
+  
+  // step through the array - starting at 1 to solve for the first character
+  for (let i=1; i<=size; i++) {
+    // if i is an even number (the modulo of i by 2 has a remainder of 0)
+    if (i % 2 === 0){
+      // add a 0 to the end of result
+      result.push(0); 
+    } else {
+      // else i is an odd number, add 1 to the end of result array
+      result.push(1)
+    }
+  }
+  // return the result array as a joined string (no spaces between elements)
+  return result.join(''); 
+}
+
+/* ------------------------------- Reflection ------------------------------- */
+// ? This solution works, but what does it look like from a space and time standpoint? 
+//* We're creating a variable, an array, entering a for and nested if loop and adding to the array, and then mutating the array with .join
+
