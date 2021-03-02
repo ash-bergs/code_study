5885c2de33227c6e648a8c36e745059cc92225e0
Ash <65979049+ashley-bergsma@users.noreply.github.com>
2021-03-02 01:40:32 -0800

    Function oddOrEven
    Done 2 ways - brute force and in-place - refactored and refreshed on some array methods

--- num_challenges.js
+++ num_challenges.js
@@ -77,3 +77,41 @@ function oneLinePositiveSum(arr) {
     // using an arrow function in the reduce method (and the .filter), we drop the `return` keyword in the function block (as well at the {} block bracers)
     return arr.filter(x => x >= 0).reduce((a,b) => a + b, 0); 
 }
+
+// ❓ Odd or Even 
+
+/* ------------------------------- description ------------------------------ */
+/* 
+
+*/
+
+function oddOrEven(array) {
+    // base case - arr is 0 if empty, and therefore even 
+   if (array.length == 0) return "even"; 
+   // use .reduce on array to sum all the numbers 
+   const sum = array.reduce((a, b) => {
+     return a + b;
+   }, 0); 
+   // check return of .reduce (the sum of all numbers) for evenness 
+   if (sum % 2 == 0){
+     return "even"; 
+   } else return "odd"; 
+}
+
+/* ------------------------------- Reflection ------------------------------- */
+//! I can definitely make this into an one-line, concise answer by using a ternary statement! 
+// We can also cut down on space/time complexity by doing things in-place 
+
+function refactoredOddOrEven(arr) {
+    return arr.reduce((a,b) => a + b, 0) % 2 ? "odd" : "even" 
+}
+
+// Stepping through return:
+// 1 - .reduce - reduce executes a provided callback fn on every el of the arr 
+// callback function is summing the accumulator (a) (starts at 0) and the currentValue (current el of arr being processed)
+// returns SINGLE value of all summed integers
+
+// 2 - % 2 ? "odd" : "even"
+// asks the question: is the value of the result of the reduce function divided by 2 truthy? i.e. does it have value?
+// because 0 is considered falsey, and any other numerical value is considered truthy, the "odd" return will be the truthy clause of the ternary statement 
+ 
