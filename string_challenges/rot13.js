b5708c7a1dc24c0db10ae0d9da9d012e9c1732ad
Ash <ashley.bergsma42@gmail.com>
2021-03-13 00:57:16 -0700

    function rot13

--- string_challenges/rot13.js
+++ string_challenges/rot13.js
@@ -8,19 +8,12 @@ Create a function that takes a string and returns the string ciphered with Rot13
 
 //* FIST ROUND SOLUTION 
 
-function rot13(message){
-    // make a dictionary of each letters ciphered value? 
-    
-    // numbers & spec chars remain as they are 
-    
-    // iterate through the string, checking each element & updating as needed 
-    
-  }
-
   function rot13(s) {
       // regex [A-Z] looks for all alphabetic chars, globally, and case insensitive 
     return s.replace(/[A-Z]/gi, c =>
         // instead of defining 1 'replacer', the replacer is given as an arrow function
       "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"[
+          // this bracket notation uses .indexOf method to resolve to replacer index
+          // hard coded in the 13-char shifted alphabet
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".indexOf(c) ] )
   }
