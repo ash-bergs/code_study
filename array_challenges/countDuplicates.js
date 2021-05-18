dfc89f0249141806362fe0cecd2a246bef3d7adf
Ash <ashley.bergsma42@gmail.com>
2021-05-18 10:54:42 -0700

    function countDuplicates(text)
    Returned to this problem that I *almost* forgot about

--- array_challenges/countDuplicates.js
+++ array_challenges/countDuplicates.js
@@ -18,8 +18,22 @@
 // then iterate through the resulting array, counting the number of times chars occur 
 // ? I could probably use more than one array method here, but 🌈array.prototype.map🌈 is the first one that comes to mind 
 
-function countDuplicated(str) {
-    const letters = str.split(""); 
-
-    
+function duplicateCount(text){
+    let count = 0; 
+    let buffer = ''; 
+    // sort the string before stepping through it 
+    let sortedStr = text.toLowerCase().split('').sort().join(''); 
+  
+    for(var i = 0; i < sortedStr.length; i++) {
+      // with the string sorted we can compare the elements sort of like a linked list
+      // IF the current element is the SAME as the one next to it -> increment count and continue
+      if (sortedStr[i] == sortedStr[i+1]  && sortedStr[i] != buffer) {
+          // if there is more than 1 repeat we don't want to count that, thus the check `sortedStr[i] != buffer` 
+        // if the current element is the same as the last, continue iterating without incrementing 
+        buffer = sortedStr[i];
+        count++;
+        i++;
+      }
+    }
+    return count;
 }
