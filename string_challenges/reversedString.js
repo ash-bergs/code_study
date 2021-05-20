13aa8d937f49ff145696ac9b14f797b9e66bda65
Ash <ashley.bergsma42@gmail.com>
2021-05-20 12:32:20 -0700

    Update reversedString.js
    Revisiting this problem as I work through the Coding Interview Bootcamp udemy course 😊

--- string_challenges/reversedString.js
+++ string_challenges/reversedString.js
@@ -15,3 +15,20 @@ function solution(str){
     // join the reversed characters and return as one string - .join('')
     return str.split('').reverse().join(''); 
 }
+
+//* The "With a Little Help From Google" approach 
+//? What if we can't use that oh-so-handy reverse method?
+
+function reverse(str) {
+    // using 🌈for loop🌈
+    // no need to cast str into an array and then back into a string! 
+    let reversedStr = ""; 
+
+    // use a for loop to step *backwards* through the array 
+    for (let i = str.length - 1; i >= 0; i--) { 
+        // concatenate current index to reversedStr
+        reversedStr += str[i]; 
+    }
+
+    return reversedStr; 
+}
